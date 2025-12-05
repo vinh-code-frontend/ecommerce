2d640fc19d21a020c292fd7bb5f9b2ba9b5ab0ac
Nguyen Le Vinh <vikvenus199x@gmail.com>
2025-12-05 21:43:34 +0700

    remove express code base

--- api/express/src/controllers/hello.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const getHello = (req: Request, res: Response) => {
-  res.json({
-    message: "Xin chào! Express + TypeScript + Alias đang hoạt động tốt.",
-    timestamp: new Date().toISOString(),
-  });
-};
